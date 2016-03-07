404048815487c9d3aee188eab3b505cf2d3eea86
binghuo365 <binghuo365@hotmail.com>
2016-03-07 11:29:27 +0800

    add test

--- Test.cpp
+++ Test.cpp
@@ -6,16 +6,22 @@ class object
 	: public CRefShared
 {
 public:
-	object(){}
-	~object(){}
+	object() : i(0)
+	{
+		std::cout << "construct" << std::endl;
+	}
+	~object()
+	{
+		std::cout << "deconstruct" << std::endl;
+	}
 
 	void printHelloWorld()
 	{
 		std::cout << "hello world" << std::endl;
 	}
 
-private:
-
+public:
+	int i;
 };
 
 typedef CHandle<object> objectPtr;
@@ -23,13 +29,23 @@ typedef CHandle<object> objectPtr;
 int main()
 {
 	objectPtr obj = new object();
+	obj->i = 1;
 	objectPtr obj2 = obj;
 	obj->printHelloWorld();
 	objectPtr obj3 = new object();
+	obj3->i = 3;
 	std::vector<objectPtr> vecObj;
 	object ob;
 	ob.printHelloWorld();
 	vecObj.push_back(obj);
 	vecObj.push_back(obj3);
+
+	obj = obj3;
+	std::cout << obj->i << std::endl;
+
+	*obj = *obj3;
+	std::cout << obj->i << std::endl;
+
+	system("pause");
 	return 0;
 }
